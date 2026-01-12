0cf47b142a8e2ff5bb3ab59b1c85f53496be4290
DonBatman <serfdon@gmail.com>
2026-01-12 04:24:49 -0800

    Added Lucky Block Support

--- init.lua
+++ init.lua
@@ -8,3 +8,12 @@ dofile(core.get_modpath("mydeck").."/beam.lua")
 dofile(core.get_modpath("mydeck").."/stairs.lua")
 dofile(core.get_modpath("mydeck").."/stain.lua")
 dofile(core.get_modpath("mydeck").."/lattice.lua")
+
+
+if core.get_modpath("lucky_block") then
+	lucky_block:add_blocks({
+		{"dro", {"mydeck:machine"}, 1},
+		{"dro", {"mydeck:deck_boards"}, 35},
+		{"dro", {"mydeck:rail"}, 20},
+	})
+end
